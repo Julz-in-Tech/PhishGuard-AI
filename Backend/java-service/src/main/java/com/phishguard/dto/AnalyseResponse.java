1995b5d4d79c461a4a10124f398f332cca53b763
Julia Moyo <juliamoyo13@gmail.com>
2026-09-17 15:23:16 +0200

    Fixed file destination mismatch

--- Backend/java-service/src/main/java/com/phishguard/dto/AnalyseResponse.java
+++ Backend/java-service/src/main/java/com/phishguard/dto/AnalyseResponse.java
@@ -1,9 +1,13 @@
 package com.phishguard.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnalyzeResponse {
     private int score;
     private String verdict;
